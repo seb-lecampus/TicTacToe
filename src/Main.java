c795eaad6f2b9899770ca170947a4f48429a3caf
Seb <lecampus@users.noreply.github.com>
2024-01-16 17:04:02 +0100

    wip

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,15 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] Args){
-        TicTacToe ticTacToe = new TicTacToe(3, 3, new Player('X'));
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Joueur 1: ");
+        char p1 = input.nextLine().charAt(0);
+        System.out.println("Joueur 2: ");
+        char p2 = input.nextLine().charAt(0);
+
+        TicTacToe ticTacToe = new TicTacToe(3, 3, new Player(p1), new Player(p2));
         ticTacToe.play();
     }
 }
--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -1,3 +1,6 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class TicTacToe {
     private final int sizeX;
     private final int sizeY;
@@ -5,8 +8,9 @@ public class TicTacToe {
     private Cell[][] board;
 
     private final Player p1;
+    private final Player p2;
 
-    TicTacToe(int sizeX, int sizeY, Player p1) {
+    TicTacToe(int sizeX, int sizeY, Player p1, Player p2) {
         this.sizeX = sizeX;
         this.sizeY = sizeY;
         this.board = new Cell[sizeY][sizeX];
@@ -14,10 +18,26 @@ public class TicTacToe {
                 for(int j=0; j < board[i].length; ++j)
                     board[i][j] = new Cell();
         this.p1 = p1;
+        this.p2 = p2;
     }
 
     public void play(){
-        display();
+        Player current = p1;
+
+        int[] move;
+        do {
+            display();
+            do {
+                move = getMoveFromPlayer(current);
+                if(isValidMove(move))
+                    break;
+                else
+                    System.out.println("Invalid case try again");
+            } while (true);
+
+            this.board[move[1]][move[0]].setOwner(current); // set owner
+            current = (current == p1) ? p2 : p1;            // change current player
+        } while(isEnd(move));
     }
 
     public void display(){
@@ -29,11 +49,66 @@ public class TicTacToe {
         }
     }
 
-    public int[] getMoveFromPlayer(int x, int y){
+    public int[] getMoveFromPlayer(Player p){
+        int x, y;
+        Scanner input = new Scanner(System.in);
+
+        System.out.printf("Joueur %c coup x: ", p.getRepresentation());
+        x = input.nextInt();
+        System.out.printf("Joueur %c coup y: ", p.getRepresentation());
+        y = input.nextInt();
+
+        return new int[]{x, y};
+    }
+
+    public Boolean isOnBoard(int[] move){
+        int x = move[0];
+        int y = move[1];
+
+        if(!(x >= 0 && x < sizeX))
+            return false;
+        if(!(y >= 0 && y < sizeY))
+            return false;
+
+        return true;
+    }
+
+    public Boolean isValidMove(int[] move) {
         // Is on board ?
-        x = (x >= 0 && x < sizeX) ? x : -1;
-        y = (y >= 0 && y < sizeY) ? y : -1;
+        isOnBoard(move);
         // Is owned ?
-        return (this.board[y][x].getOwner() == null) ? new int[]{x, y} : new int[]{-1, -1};
+        if(this.board[move[1]][move[0]].getOwner() != null)
+            return false;
+
+        return true;
+    }
+
+    public Boolean isEnd(int[] move){
+        if(checkfull()){
+            System.out.println("execo");
+            return false;
+        }
+        return true;
+    }
+
+    private Boolean checkDir(int[] move, int[] dir, int consecutive) {
+        Player owner = this.board[move[1]][move[0]].getOwner();
+        for(int i=0; i < consecutive; ++i){
+            if(!isOnBoard(move))
+                return false;
+            if(this.board[move[1]][move[0]].getOwner() != owner)
+                return false;
+            move[0] += dir[0];
+            move[1] += dir[1];
+        }
+        return true;
+    }
+
+    private Boolean checkfull() {
+        for(var line : this.board)
+            for(Cell cell : line)
+                if(cell.getOwner() == null)
+                    return false;
+        return true;
     }
 }
