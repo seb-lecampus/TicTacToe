3288bbd90b02d7c51e78759411b67366c677c01a
Seb <lecampus@users.noreply.github.com>
2024-01-19 12:13:27 +0100

    use one scanner

--- src/InteractionUtilisateur.java
+++ src/InteractionUtilisateur.java
@@ -2,42 +2,47 @@ import java.util.Scanner;
 
 public class InteractionUtilisateur {
     private View view;
+    private Scanner scanner = new Scanner(System.in);
 
     public InteractionUtilisateur(View view){
         this.view = view;
     }
 
     public char askRepresentation() {
-        Scanner input = new Scanner(System.in);
         view.ask_for_player_representation();
-        return input.nextLine().charAt(0);
+        return scanner.nextLine().charAt(0);
     }
 
     public int[] askMoveFromPlayer(PlayerHuman p) {
         int x, y;
-        Scanner input = new Scanner(System.in);
+
 
         do {
             try {
                 view.ask_coordinate_for_player('X', p);
 
-                x = input.nextInt();
+                x = scanner.nextInt();
                 break;
             } catch (Exception e){
-                view.warn_number_only(); input.next();
+                view.warn_number_only();
             }
         }while(true);
 
         do {
             try {
                 view.ask_coordinate_for_player('Y', p);
-                y = input.nextInt();
+                y = scanner.nextInt();
                 break;
             } catch (Exception e){
-                view.warn_number_only(); input.next();
+                view.warn_number_only(); scanner.next();
             }
         }while(true);
 
         return new int[]{x, y};
+    }
+
+    private static void clear_scanner(Scanner scanner){
+        while(scanner.hasNext())
+            scanner.next();
     }
 }
